4c06d59a645f4f9cc6a413629aa493337ce865d5
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-06-07 14:16:33 -0700

    bump version to 2.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures jupyterhub-chef'
 long_description 'Installs/Configures jupyterhub-chef'
-version '1.1.0'
+version '2.0.0'
 
 depends 'build-essential'
