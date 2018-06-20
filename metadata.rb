bc3241f02219d0645364b720ab49cdfbd5d361a0
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-06-20 13:33:46 -0700

    update metas

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@imnorobot.com'
 license 'MIT'
 description 'Installs/Configures jupyterhub-chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.0.0'
+version '0.2.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 source_url 'https://github.com/hansohn/jupyterhub-chef' if respond_to?(:source_url)
 issues_url 'https://github.com/hansohn/jupyterhub-chef/issues' if respond_to?(:issues_url)
