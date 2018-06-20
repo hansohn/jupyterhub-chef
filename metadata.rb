87744daa237657f6b8497d4ab19de380c3aaa3cc
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-06-20 14:07:51 -0700

    update cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@imnorobot.com'
 license 'MIT'
 description 'Installs/Configures jupyterhub-chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '2.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 source_url 'https://github.com/hansohn/jupyterhub-chef' if respond_to?(:source_url)
 issues_url 'https://github.com/hansohn/jupyterhub-chef/issues' if respond_to?(:issues_url)
