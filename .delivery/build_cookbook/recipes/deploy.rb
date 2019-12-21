1ad3c639fb3ba710c5cbb75b49121a85d2c4e873
Ryan Hansohn <hansohn@users.noreply.github.com>
2019-12-21 14:30:58 -0800

    cookstyle updates

--- .delivery/build_cookbook/recipes/deploy.rb
+++ .delivery/build_cookbook/recipes/deploy.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: build_cookbook
+# Cookbook:: build_cookbook
 # Recipe:: deploy
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
--- .delivery/build_cookbook/recipes/functional.rb
+++ .delivery/build_cookbook/recipes/functional.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: build_cookbook
+# Cookbook:: build_cookbook
 # Recipe:: functional
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
