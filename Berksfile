0cf9ba8598c328d6e23da6b5a894366b9d941345
Ryan Hansohn <hansohn@users.noreply.github.com>
2019-12-21 14:26:27 -0800

    bump version

--- Berksfile
+++ Berksfile
@@ -3,3 +3,5 @@ source 'https://supermarket.chef.io'
 metadata
 
 cookbook 'alternatives', '~> 0.2.0'
+cookbook 'yum-epel'
+cookbook 'python-chef', '~> 0.2.2', git: 'https://github.com/hansohn/python-chef.git'
--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,12 @@ maintainer 'Ryan Hansohn'
 maintainer_email 'info@imnorobot.com'
 license 'MIT'
 description 'Installs/Configures jupyterhub-chef'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.3.0'
-chef_version '>= 12.14' if respond_to?(:chef_version)
-source_url 'https://github.com/hansohn/jupyterhub-chef' if respond_to?(:source_url)
-issues_url 'https://github.com/hansohn/jupyterhub-chef/issues' if respond_to?(:issues_url)
+version '3.0.0'
+chef_version '>= 12.14'
+source_url 'https://github.com/hansohn/jupyterhub-chef'
+issues_url 'https://github.com/hansohn/jupyterhub-chef/issues'
 
 supports 'centos', '>= 7.0'
 
 depends 'alternatives'
+depends 'python-chef'
